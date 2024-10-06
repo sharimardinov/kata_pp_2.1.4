f7c219d411e6c7800005ac9c1308101373659f54
sharimardinov <sharimardinov@icloud.com>
2024-10-06 15:01:47 +0300

    kata_pp_2.1.4

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    private String id;
+
+    @Autowired
+    public Deth8() { this.id = " смерть Кощея на игле :("; }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+
 }
--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,16 +1,18 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
 
+    @Qualifier("island2")
     @Autowired
     private Island2 island;
 
     @Override
     public String toString() {
-        return "на океане остров " + island.toString();
+        return "На свете есть океан ," + island.toString();
     }
 }
